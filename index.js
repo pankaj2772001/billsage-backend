8307bebc7bc05b5143d4fdc0b1b5778fed2937dd
Pankaj Gupta <pankaj392pg@gmail.com>
2025-08-22 17:24:52 +0530

    fix: index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const multer = require("multer");
 
 const fs = require("fs");
 
-const uploadDir = "./uploads";
+const uploadDir = "/tmp";
 
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
